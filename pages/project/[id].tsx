2d77ed3e2dbb9cab38b6cea70dac1210d7a4e202
rei <reiballa@gmail.com>
2022-05-27 01:54:58 +0200

    lets fix this

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -10,20 +10,26 @@ import { getSupabase } from "../../utils/supa.";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import { CopyToClipboard } from "react-copy-to-clipboard"
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { FaAngleLeft as LeftArrow } from "react-icons/fa"
+
+interface contextValue {
+  copied: boolean,
+  setCopied: Function
+}
+const CopiedContext = createContext<contextValue>({ copied: false, setCopied: () => console.log("test") })
 
 const normaltext = (props: any) => {
   const { children } = props
   return (
-    <Text fontSize="1.2em" color="gray.700">{children}</Text>
+    <Box fontSize="1.2em" color="gray.700">{children}</Box>
   )
 }
 
 const litext = (props: any) => {
   const { children } = props
   return (
-    <Text fontSize="1.2em" color="gray.700" _before={{ content: "'- '" }}>{children}</Text>
+    <Box fontSize="1.2em" color="gray.700" _before={{ content: "'- '" }}>{children}</Box>
   )
 }
 
@@ -33,31 +39,31 @@ const markTheme = {
   h1: (props: any) => {
     const { children } = props
     return (
-      <Text fontSize={{ md: "3xl", base: "xl"}} fontWeight="bold" color="gray.700" my="2">{children}</Text>
+      <Box fontSize={{ md: "3xl", base: "xl" }} fontWeight="bold" color="gray.700" my="2">{children}</Box>
     )
   },
   h2: (props: any) => {
     const { children } = props
     return (
-      <Text fontSize={{ md: "2xl", base: "lg" }} fontWeight="bold" color="gray.700" my="2">{children}</Text>
+      <Box fontSize={{ md: "2xl", base: "lg" }} fontWeight="bold" color="gray.700" my="2">{children}</Box>
     )
   },
   h3: (props: any) => {
     const { children } = props
     return (
-      <Text fontSize={{ md: "xl", base: "md" }} fontWeight="bold" color="gray.700" my="2">{children}</Text>
+      <Box fontSize={{ md: "xl", base: "md" }} fontWeight="bold" color="gray.700" my="2">{children}</Box>
     )
   },
   strong: (props: any) => {
     const { children } = props
     return (
       <chakra.span fontWeight="semibold" color="gray.700">{children}</chakra.span>
-    ) 
+    )
   },
   blockquote: (props: any) => {
     const { children } = props
     return (
-      <Text fontFamily="monospace" fontSize="lg" p="2" bg="blackAlpha.50" borderLeft="8px solid" borderLeftColor="gray.500" m="5">{children}</Text>
+      <Box fontFamily="monospace" fontSize="lg" p="2" bg="blackAlpha.50" borderLeft="8px solid" borderLeftColor="gray.500" m="5">{children}</Box>
     )
   },
   code: (props: any) => {
@@ -69,74 +75,83 @@ const markTheme = {
   pre: (props: any) => {
 
     const { children } = props
-    const [copied, setCopied] = useState(false)
-
-    useEffect(() => {
-      setTimeout(() => {
-        setCopied(false)
-      }, 1000 * 10)
-    }, [copied])
+    const { copied, setCopied } = useContext(CopiedContext)
 
     return (
       <Box rounded="lg" m="5" width="90%" position="relative">
         <SyntaxHighlighter showLineNumbers lineNumberStyle={{ color: ZIMA, borderRight: `1px solid ${ZIMA}`, marginRight: "1em" }} language="javascript" style={vscDarkPlus}>
-          {children[0].props.children} 
+          {children[0].props.children}
         </SyntaxHighlighter>
-        
+
         {
-          copied ? 
-          <Button position="absolute" top="2" right="2" variant="link" colorScheme="whiteAlpha" fontSize={14} disabled>Copied</Button>
-          : 
-          <CopyToClipboard text={""} onCopy={() => setCopied(true)}>
-            <Button position="absolute" top="2" right="2" variant="link" colorScheme="whiteAlpha" fontSize={14} onClick={() => console.log(children[0].props.children)}>Copy</Button>
-          </CopyToClipboard> 
+          copied ?
+            <Button position="absolute" top="2" right="2" variant="link" colorScheme="whiteAlpha" fontSize={14} disabled>Copied</Button>
+            :
+            <CopyToClipboard text={children[0].props.children.toString()} onCopy={() => setCopied(true)}>
+              <Button position="absolute" top="2" right="2" variant="link" colorScheme="whiteAlpha" fontSize={14}>Copy</Button>
+            </CopyToClipboard>
         }
       </Box>
-    ) 
+    )
   }
 }
 
-const Project: NextPage<{project: Project}> = ({ project }) => {
+const isCopied = (e: Event) => {
+  e.preventDefault()
+  return true
+}
+
+const Project: NextPage<{ project: Project }> = ({ project }) => {
 
   const niceDate = (date: string) => {
     const d = new Date(date)
     const nicedate = `${d.toLocaleDateString("default", { month: "short" })}, ${d.toLocaleString("default", { day: "numeric" })} ${d.getFullYear()}`
     return nicedate
-  } 
+  }
+
+  const [copied, setCopied] = useState(false)
+
+  useEffect(() => {
+
+    window.addEventListener("copy", isCopied)
+
+  }, [copied])
 
   return (
-    <Stack mt="4" width={{ md: "container.md", base: "container.sm" }} margin="auto">
-      {/* <Text fontSize={{ md: "4xl", base: "2xl" }} fontWeight="bold" align="center" color="gray.700">{project?.title}</Text>
+    <CopiedContext.Provider value={{ copied, setCopied }}>
+      <Stack mt="4" width={{ md: "container.md", base: "container.sm" }} margin="auto">
+        {/* <Text fontSize={{ md: "4xl", base: "2xl" }} fontWeight="bold" align="center" color="gray.700">{project?.title}</Text>
       <HStack alignSelf="center">
         <Badge fontSize={{ md: "md", base: "sm" }} fontWeight="medium" textAlign="center" fontStyle="italic" bg="blackAlpha.100" color={ZIMA}>{niceDate(project?.created_at || "")}</Badge>
         <Text fontSize={{ md: "xl", base: "sm" }} fontWeight="hairline" align="center">{project?.description}</Text>
       </HStack> */}
-      <VStack align="flex-start" ml="4">
-        <Link href="/">
-          <ChakraLink _hover={{ color: ZIMA }}>
-            <HStack>
-              <LeftArrow />
-              <Text>Go back</Text>
-            </HStack>
-          </ChakraLink>
-        </Link>
-      </VStack>
-      <Box alignSelf="center" h={{ md: "50vh", base: "100%" }} w={{ md: "45vw", base: ""}}><Image boxSize="full" objectFit="contain" src={project?.thumbnail} /></Box>
-      <Box width={{ md: "container.md", base: "container.sm" }} alignSelf="center" p="0" paddingLeft="4" paddingRight="4">
-        <VStack align="flex-start">
-          <Text fontSize={{ md: "md", base: "sm"}} color="gray.500" fontWeight="semibold">{project.description}</Text>
-          <Text fontSize={{ md: "md", base: "sm"}} color="blue.700" fontWeight="semibold">{niceDate(project.created_at)}</Text>
+        <VStack align="flex-start" ml="4">
+          <Link href="/">
+            <ChakraLink _hover={{ color: ZIMA }}>
+              <HStack>
+                <LeftArrow />
+                <Text>Go back</Text>
+              </HStack>
+            </ChakraLink>
+          </Link>
         </VStack>
-        <ReactMarkdown components={ChakraUIRenderer(markTheme)} remarkPlugins={[remarkGfm]}>{project?.content || ""}</ReactMarkdown> 
-      </Box>
-    </Stack>
+        <Box alignSelf="center" h={{ md: "50vh", base: "100%" }} w={{ md: "45vw", base: "" }}><Image boxSize="full" objectFit="contain" src={project?.thumbnail} /></Box>
+        <Box width={{ md: "container.md", base: "container.sm" }} alignSelf="center" p="0" paddingLeft="4" paddingRight="4">
+          <VStack align="flex-start">
+            <Text fontSize={{ md: "md", base: "sm" }} color="gray.500" fontWeight="semibold">{project.description}</Text>
+            <Text fontSize={{ md: "md", base: "sm" }} color="blue.700" fontWeight="semibold">{niceDate(project.created_at)}</Text>
+          </VStack>
+          <ReactMarkdown components={ChakraUIRenderer(markTheme)} remarkPlugins={[remarkGfm]}>{project?.content || ""}</ReactMarkdown>
+        </Box>
+      </Stack>
+    </CopiedContext.Provider>
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const supabase = getSupabase()
   const projects = await (await supabase.from("projects").select("*")).data
-  
+
   //@ts-ignore
   const paths = projects.map((p) => ({ params: { id: p.id.toString() } }))
   return {
@@ -149,7 +164,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const supabase = getSupabase()
   const project = await (await supabase.from("projects").select("*").eq("id", params?.id)).data
 
-  if(!project) {
+  if (!project) {
     return {
       props: {
         project: {}
